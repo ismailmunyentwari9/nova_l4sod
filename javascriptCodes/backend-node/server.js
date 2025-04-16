afca2e100c277bdb7cfba913de9372d8613e591d
Isma-ui <kidzoncodes.com>
2025-04-16 12:28:21 +0200

    Add noded code

---
+++ javascriptCodes/backend-node/server.js
@@ -0,0 +1,22 @@
+const http= require('http');
+// .....Create Server........
+const server = http.createServer((req,resp)=>{
+//    resp.end("Hello, I'm from server")
+   console.log(req.url);
+   if(req.url ==='/'){
+    resp.end("This Is Index Page");
+   }
+   else if(req.url ==='/about'){
+    resp.end("This Is About Page");
+   }
+   else if(req.url ==='/contact'){
+    resp.end("This Is Contact Page");
+   }
+   else{
+    resp.end("404 Error")
+   }
+   
+
+})
+// ........Listern on port 5000;
+server.listen(5000,console.log("I'm listerning on port 5k"));
